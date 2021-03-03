bb8223783f35d60ec129e9e03f04d0c994f74648
yfchen12 <ych015@aucklanduni.ac.nz>
2021-03-03 13:15:16 +1300

    refactor output format

--- AccountingMetrics/AccountingMetrics/Program.cs
+++ AccountingMetrics/AccountingMetrics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace AccountingMetrics
 {
@@ -8,19 +9,19 @@ namespace AccountingMetrics
         {
             var metrics = new Metrics(new AccountRepository("../../../../../data.json"));
             var revenue = metrics.CalRevenue();
-            Console.WriteLine($"Revenue: {revenue}");            
+            Console.WriteLine($"Revenue: {revenue.ToString("c0")}");            
             
             var expenses = metrics.CalExpenses();
-            Console.WriteLine($"expenses: {expenses}");
+            Console.WriteLine($"expenses: {expenses.ToString("c0")}");
 
             var grossMargin = metrics.CalGrossProfitMargin(revenue);
-            Console.WriteLine($"Gross Profit Margin: {grossMargin}");
+            Console.WriteLine($"Gross Profit Margin: {(grossMargin).ToString("p0")}");
 
             var netMargin = metrics.CalNetProfitMargin(revenue, expenses);
-            Console.WriteLine($"Gross Profit Margin: {netMargin}");
+            Console.WriteLine($"Gross Profit Margin: {(netMargin).ToString("p0")}");
 
             var workCapRatio = metrics.CalWorkingCapitalRatio();
-            Console.WriteLine($"Working Capital Ratio: {workCapRatio}");
+            Console.WriteLine($"Working Capital Ratio: {(workCapRatio).ToString("p0")}");
 
         }
     }
