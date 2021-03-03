61831d985a0a9d6361ab4ce4488772baa5b5cf27
yfchen12 <ych015@aucklanduni.ac.nz>
2021-03-03 14:42:34 +1300

    refactor to add constructor to setup test data before all test

--- AccountingMetrics/AccountingMetrics.Tests/MetricsTest.cs
+++ AccountingMetrics/AccountingMetrics.Tests/MetricsTest.cs
@@ -5,20 +5,24 @@ namespace AccountingMetrics.Tests
 {
     public class MetricsTest
     {
+        private readonly Metrics _metrics;
+        public MetricsTest()
+        {
+            _metrics = new Metrics(new FakeAccountRepository());
+        }
         [Fact]
         public void testCalRevenue()
         {
-            var metrics = new Metrics(new FakeAccountRepository());
-            float actual = metrics.CalRevenue();
+            float actual = _metrics.CalRevenue();
             Assert.Equal(3, actual);
         }
 
         [Fact]
         public void testCalExpenses()
         {
-            var metrics = new Metrics(new FakeAccountRepository());
-            float actual = metrics.CalExpenses();
+            float actual = _metrics.CalExpenses();
             Assert.Equal(7, actual);
         }
+
     }
 }
